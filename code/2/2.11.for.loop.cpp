1eff78281d684199041be17ac61b1929b4b0a6ab
caesar <18709447846@163.com>
2023-06-07 09:35:19 +0800

    private modification

--- code/2/2.11.for.loop.cpp
+++ code/2/2.11.for.loop.cpp
@@ -1,24 +1,44 @@
-//
-// 2.11.for.loop.cpp
-// chapter 2 language usability
-// modern cpp tutorial
-//
-// created by changkun at changkun.de
-// https://github.com/changkun/modern-cpp-tutorial
-//
+// //
+// // 2.11.for.loop.cpp
+// // chapter 2 language usability
+// // modern cpp tutorial
+// //
+// // created by changkun at changkun.de
+// // https://github.com/changkun/modern-cpp-tutorial
+// //
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// int main() {
+//     std::vector<int> vec = {1, 2, 3, 4};
+//     if (auto itr = std::find(vec.begin(), vec.end(), 3); itr != vec.end()) *itr = 4;
+//     for (auto element : vec)
+//         std::cout << element << std::endl; // read only
+//     for (auto &element : vec) {
+//         element += 1;                      // writeable
+//     }
+//     for (auto element : vec)
+//         std::cout << element << std::endl; // read only
+// }
+
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-int main() {
+int main() 
+{
     std::vector<int> vec = {1, 2, 3, 4};
-    if (auto itr = std::find(vec.begin(), vec.end(), 3); itr != vec.end()) *itr = 4;
-    for (auto element : vec)
-        std::cout << element << std::endl; // read only
-    for (auto &element : vec) {
-        element += 1;                      // writeable
+
+if (const std::vector<int>::iterator itr = std::find(vec.begin(), vec.end(), 3);
+    itr != vec.end()) 
+    {
+    *itr = 4;
     }
-    for (auto element : vec)
-        std::cout << element << std::endl; // read only
+
+for (std::vector<int>::iterator element = vec.begin(); element != vec.end(); 
+    ++element)
+    std::cout << *element << std::endl;
 }
